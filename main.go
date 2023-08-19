a90f05976c70c4d68897f0bb7ded68871b1e02e1
Omar <88511754+Jamlee977@users.noreply.github.com>
2023-08-19 20:55:29 +0300

    Fixed a bug that didn't allow to execute a command with no arguments

--- main.go
+++ main.go
@@ -13,16 +13,17 @@ func main() {
     args := argsArray[0]
     cmdArgs := argsArray[1:]
 
-    if len(cmdArgs) == 0 {
-        cmdArgs = []string{""}
-    }
-
     start := time.Now().UnixMilli()
     cmd := exec.Command(args, cmdArgs...)
     end := time.Now().UnixMilli()
 
     out, err := cmd.CombinedOutput()
     if err != nil {
+        if len(out) > 0 {
+            fmt.Printf("%s\n", out)
+            fmt.Printf("Time: %.3f\n", float64(end - start) / 1000)
+            return
+        }
         fmt.Printf("Error: %s\n", err)
         return
     }
